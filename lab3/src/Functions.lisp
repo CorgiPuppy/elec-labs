a0350ed521d9301edd521f3a11be0cde816c4ef7
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2024-10-12 23:42:16 +0300

    fix: add calculations for the first lab and fix bugs.

--- lab3/src/Functions.lisp
+++ lab3/src/Functions.lisp
@@ -3,8 +3,19 @@
 	(loop for i from begin-index to end-index and value from value-name by step-value do
 		(cond
 			((string-equal physical-value "C_p") (setf (aref array-name-1 i) value))
-			((string-equal physical-value (or "Y" "G" "B")) (setf (aref array-name-1 i) (/ (aref array-name-2 i) U)))
+			((or (string-equal physical-value "R") 
+				 (string-equal physical-value "X_L")
+				 (string-equal physical-value "X_C")) 
+			 (setf (aref array-name-1 i) (/ (aref array-name-2 i) (aref array-name-3 i))))
+			((string-equal physical-value "Z") (setf (aref array-name-1 i) (/ U (aref array-name-2 i))))
+			((or (string-equal physical-value "X")
+				 (string-equal physical-value "B")) 
+			 (setf (aref array-name-1 i) (- (aref array-name-2 i) (aref array-name-3 i))))
+			((string-equal physical-value "phi") (setf (aref array-name-1 i) (atan (/ (aref array-name-2 i) (aref array-name-3 i)))))
 			((string-equal physical-value "S") (setf (aref array-name-1 i) (* (aref array-name-2 i) U)))
+			((or (string-equal physical-value "Y")
+				 (string-equal physical-value "G")
+				 (string-equal physical-value "B_L")
+				 (string-equal physical-value "B_C")) 
+			 (setf (aref array-name-1 i) (/ (aref array-name-2 i) U)))
 			((string-equal physical-value "cos_phi") (setf (aref array-name-1 i) (/ (aref array-name-2 i) (aref array-name-3 i)))))))
-
-
