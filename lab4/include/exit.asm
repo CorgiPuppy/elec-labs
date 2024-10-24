2d3cb6a3009e1d6b06000ed3cc1011df5788ef3e
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2024-10-24 13:59:56 +0300

    delete.

--- lab4/include/exit.asm
+++
@@ -1,6 +0,0 @@
-exit:
-	mov		rbx, 0		; return 0 for exit
-	mov		rax, 1		; call sys_exit
-	int		80h
-
-	ret
---
+++ lab4/src/main.f90
@@ -0,0 +1,9 @@
+program main
+    implicit none
+
+    integer :: temp
+
+    temp = 5
+
+    print *, 'My number -', temp
+end program end
