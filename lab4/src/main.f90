a76eba18a64418becb3589e5d9ea6f5b8ea849c4
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2024-10-28 11:53:36 +0300

    fix: update.

--- lab4/src/main.f90
+++ lab4/src/main.f90
@@ -1,9 +1,19 @@
 program main
     implicit none
 
-    integer :: temp
+    real :: U_A
+    real :: U_B
+    real :: U_C
+    real :: U_nn
+    real :: U_AB
+    real :: U_BC
+    real :: U_AC
+    real :: I_A
+    real :: I_B
+    real :: I_C
+    real :: I_N
 
-    temp = 5
+    U_A = 5.0
 
-    print *, 'My number -', temp
-end program end
+    print *, "My number -", U_A
+end program
