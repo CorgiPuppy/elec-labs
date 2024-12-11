37396d14f81fdc00c2b541a2acbb8098a7666369
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2024-12-11 06:33:41 +0300

    fix: add write to file method.

--- lab4/src/main.rb
+++ lab4/src/main.rb
@@ -19,8 +19,29 @@ I_B = [3.142, 3.142, 3.142, 4.095]
 I_C = [3.143, 1.294, 3.143, 2.557]
 I_N = [0, 8.926]
 
-def calc_P(i, u)
-	return i ** 2 * u
+Folder = "plots/files/"
+
+def calc_P(u, i)
+	return u * i
 end
 
-puts calc_P(2, 3)
+def write_to_file(path)
+	
+end
+
+length = U_A.length
+p_A = Array.new(length, 0)
+p_B = Array.new(length, 0)
+p_C = Array.new(length, 0)
+p_Circuit = Array.new(length, 0)
+
+print "P_A\tP_B\tP_C\tP_Circuit\n"
+p_A.each_with_index do |element, index|
+	p_A[index] = calc_P(U_A[index], I_A[index])
+	p_B[index] = calc_P(U_B[index], I_B[index])
+	p_C[index] = calc_P(U_C[index], I_C[index])
+	p_Circuit[index] = p_A[index] + p_B[index] + p_C[index]
+	printf "%.1f\t%.1f\t%.1f\t%.1f\n", p_A[index], p_B[index], p_C[index], p_Circuit[index]
+end
+
+# file = File.open(Folder + "I
